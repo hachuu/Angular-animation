2b9d8e0f8e53f862e8236b186146f605ddbc0057
hysong <hachu.developer@gmail.com>
2022-01-20 15:41:08 +0900

    pdf 추출 및 excel 시트 그리드 말고 그냥 angular 소스로 적용

--- src/app/excel/excel.component.ts
+++ src/app/excel/excel.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { jsPDF } from 'jspdf';
+// import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
 import { ExcelService } from './excel.service';
 import { mockDataArr } from './mock/mockDataArr';
 
@@ -19,6 +23,20 @@ export class ExcelComponent implements OnInit {
   title = 'exportExcelInAngular';
   mockDataHeader: string[] | undefined;
   mockDataArr: Array<{[index: string]: string | number} > = mockDataArr;
+
+  pasteArr: string[] | undefined = [];
+  trArr :any[] = [];
+
+  @HostListener('document:paste', ['$event']) onPaste(event: ClipboardEvent) {
+    console.log('paste event', event);
+    let pastedInput: string | undefined = event.clipboardData?.getData('text/plain');//replace(/\D/g, '');
+    console.log(event.clipboardData?.files[ 0 ]);
+    console.log(pastedInput?.split('\n'));
+    console.log(pastedInput?.split('	'));
+    this.pasteArr = pastedInput?.split('	');
+    this.trArr[this.trArr.length || 0] = this.pasteArr;
+    console.log(this.pasteArr)
+  }
   
   // mockDataArr: Array<any> = mockDataArr;
   constructor(private excelService: ExcelService) {
@@ -30,17 +48,48 @@ export class ExcelComponent implements OnInit {
     this.excelService.exportAsExcelFile(this.mockDataArr, 'user_data', this.mockDataHeader);
   }
 
+  generatePdf() {
+    var pdf = new jsPDF();
+
+    pdf.setFontSize(20);
+    pdf.text('Angular PDF Table', 11, 8);
+    pdf.setFontSize(12);
+    pdf.setTextColor(99);
+
+
+    (pdf as any).autoTable({
+    head: [['ID', 'Name', 'Email', 'Profile']],
+    body: [[1, 'John', 'john@yahoo.com', 'HR'],
+      [2, 'Angel', 'angel@yahoo.com', 'Marketing'],
+      [3, 'Harry', 'harry@yahoo.com', 'Finance'],
+      [4, 'Anne', 'anne@yahoo.com', 'Sales'],
+      [5, 'Hardy', 'hardy@yahoo.com', 'IT'],
+      [6, 'Nikole', 'nikole@yahoo.com', 'Admin'],
+      [7, 'Sandra', 'Sandra@yahoo.com', 'Sales'],
+      [8, 'Lil', 'lil@yahoo.com', 'Sales']
+    ],
+    theme: 'plain',
+    didDrawCell: (data: { column: { index: any; }; }) => {
+        console.log(data.column.index)
+    }
+    })
+
+    // Open PDF document in browser's new tab
+    pdf.output('dataurlnewwindow')
+
+    // Download PDF doc  
+    pdf.save('table.pdf');
+}  
+
+
   renderListText(item: any, header: string): string | number {
-    // return '';
     return item.hasOwnProperty(header) && item[header] ? item[header] : '';
   }
 
   // j: row, i: column
   renderBold(i: number, j: number): boolean {
     const data = this.excelService.removeBorderItemIdx(this.mockDataArr);
-    // console.log(this.excelService.removeBorderItemIdx(this.mockDataArr));
     return data.some(item => {
-      // return ((item.s.r-2 < j && item.e.r >= j + 1) && item.s.c === i);
       return ((item.s.r-1 < j && item.e.r >= j + 1) && item.s.c === i);
     });
   }
