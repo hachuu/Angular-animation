6d6cc7d32a2261c625ac7dd250818ade317ad9ec
hayoung <hayoung.song@kt.com>
2025-01-21 09:53:03 +0900

    25/01/21 ng update

--- src/app/websocket/websocket.component.ts
+++ src/app/websocket/websocket.component.ts
@@ -1,4 +1,4 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+// import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { WebsocketService } from './websocket.service';
 
